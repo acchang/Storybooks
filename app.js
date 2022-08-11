df725a85e86ae8b7a2504cf382e9675b2b840a29
Andrew Chang <acchang4@gmail.com>
2022-08-11 17:17:43 -0400

    check ports

--- app.js
+++ app.js
@@ -2,10 +2,13 @@ const express = require('express')
 const dotenv = require('dotenv')
 
 // Load config
-dotenv.config({ path: './config/config.ebv'})
+dotenv.config({ path: './config/config.env'})
 
 const app = express()
 
+const PORT = process.env.PORT || 5000
 
-
-app.listen()
+app.listen(
+    PORT, 
+    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+)
