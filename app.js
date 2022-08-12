5fba41aafac857147a829b4c3518e73662047be9
Andrew Chang <acchang4@gmail.com>
2022-08-12 15:40:27 -0400

    db file

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const dotenv = require('dotenv')
+const connectDB = require('./config/db')
 
 // Load config
 dotenv.config({ path: './config/config.env'})
@@ -9,6 +10,6 @@ const app = express()
 const PORT = process.env.PORT || 5000
 
 app.listen(
-    PORT, 
+    PORT, () => {
     console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
-)
+})
