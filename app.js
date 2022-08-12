093ae8e7a32ac15190f9a19901cae6cfdb7ef832
Andrew Chang <acchang4@gmail.com>
2022-08-12 16:19:04 -0400

    hbs

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
 const express = require('express')
 const dotenv = require('dotenv')
+const morgan = require('morgan')
+const exphbs = require('express-handlebars')
 const connectDB = require('./config/db')
 
 // Load config
 dotenv.config({ path: './config/config.env'})
 
+connectDB()
+
 const app = express()
+
+// Logging
+if (process.env.NODE_ENV === 'development'){
+    app.use(morgan('dev'))
+}
+
+// Handlebars
+app.engine('.hbs', engine({extname: '.hbs'}));
+app.set('view engine', '.hbs');
+// app.set('views', './views');
+
 
 const PORT = process.env.PORT || 5000
 
