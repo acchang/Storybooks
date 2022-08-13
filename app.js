5cb44cd12d0f60bb7233e317271f0834f44c0f98
Andrew Chang <acchang4@gmail.com>
2022-08-12 20:14:39 -0400

    to 24:14 on tutorial

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const dotenv = require('dotenv')
 const morgan = require('morgan')
-const exphbs = require('express-handlebars')
+const { engine } = require('express-handlebars')
 const connectDB = require('./config/db')
 
 // Load config
@@ -17,9 +17,13 @@ if (process.env.NODE_ENV === 'development'){
 }
 
 // Handlebars
-app.engine('.hbs', engine({extname: '.hbs'}));
+app.engine('handlebars', engine({extname: '.hbs', defaultLayout: 'main'}));
 app.set('view engine', '.hbs');
 // app.set('views', './views');
+
+
+// Routes
+app.use('/', require('./routes/index'))
 
 
 const PORT = process.env.PORT || 5000
