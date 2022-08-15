3e481cd33cbbbc4e3c8681c78b90ac145fd47203
Andrew Chang <acchang4@gmail.com>
2022-08-15 15:13:52 -0400

    before npm i connect-mongodb-session

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const path = require('path');
 const express = require('express');
+// const mongoose = require('mongoose')
 const dotenv = require('dotenv');
 const morgan = require('morgan');
 const exphbs = require('express-handlebars');
 const passport = require('passport');
 const session = require('express-session');
+const MongoStore = require('connect-mongo')
 const connectDB = require('./config/db');
+const { default: mongoose } = require('mongoose');
 
 // Load config
 dotenv.config({ path: './config/config.env'})
@@ -33,6 +36,7 @@ app.set('view engine', '.hbs'); // app.set('views', './views');
 app.use(session({   secret: 'keyboard cat',
                     resave: false,
                     saveUninitialized: false,
+                    // store: new MongoStore ({ mongooseConnection: mongoose.connection })
                     /*cookie: { maxAge: 60000 }*/}))
 
 // Passport middleware
