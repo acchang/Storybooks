6e3ba333cd28f9ada30fbe787668421bdaa5e3cc
Andrew Chang <acchang4@gmail.com>
2022-08-17 19:07:30 -0400

    ensure stuff

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,34 @@
 const express = require('express')
 const router = express.Router()
+const { ensureAuth, ensureGuest } = require('../middleware/auth')
+
+// const Story = require('../models/Story')
 
 // @desc    Login/Landing page
-// @route   GET /
-
-router.get('/', (req, res)=> {
+// @route   GET / 
+router.get('/', ensureGuest, (req, res) => {
     res.render('login', {
-        layout: 'login'
+        layout: 'login',
     })
 })
 
+// @desc    Dashboard
+// @route   GET /dashboard
+// router.get('/dashboard', ensureAuth, async (req, res) => {
+//     console.log("use object in /dashboard:", req.user);
+//     try {
+//         const stories = await Story.find({user: req.user.id}).lean()
+//         res.render('dashboard', {
+//         name: req.user.firstName,
+//         stories
+//     })
+//     } catch (err) {
+//         console.error(err)
+//         res.render('error/')
+//     }
+// })
 
-// @desc    Dashbard
-// @route   GET / dashbard
-router.get('/dashboard', (req,res) => {
+router.get('/dashboard', ensureAuth, async (req, res) => {
     res.render('dashboard')
 })
 
